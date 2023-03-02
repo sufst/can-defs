70d1e3460f09f10c0591b865323c5fb9c49c4f75
Tim Brewis <timbrewis27@gmail.com>
2023-03-02 16:47:55 +0000

    release: v0.0.5 (#20)

--- out/can_handlers.h
+++ out/can_handlers.h
@@ -2,14 +2,18 @@
 #ifndef CAN_HANDLERS_H
 #define CAN_HANDLERS_H
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
+
+#define CAN_HANDLERS_TABLE_SIZE 4
 
 /**
  * @brief   Entry in CAN handler table
  */
 typedef struct {
     uint32_t identifier;
+    uint32_t pdu_id;
     void (*unpack_func)(uint8_t*, const uint8_t*, size_t);
 } can_handler_t;
 
@@ -17,6 +21,5 @@ typedef struct {
  * function prototypes
  */
 const can_handler_t* can_handler_get(uint32_t index);
-uint32_t can_handler_table_size();
 
 #endif
